66b990a798600609dacc043f413e144eaa65f5cd
Citlalyl <43544097+Citlalyl@users.noreply.github.com>
2019-03-20 10:07:23 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//spell #1
+
+$("#tea").click(function(){
+ $("#travis").fadeToggle();
+});
+
+
+
+//spell #2
+
+$("#pen").click(function(){
+ $("#world").slideToggle();
+});
+
+
+
+//spell #3
+
+$("#Cookie").click(function(){
+ $("#pic").toggle();
+});
+
+
+
